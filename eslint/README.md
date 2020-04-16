a95fcf4a4b4107ff7cac24cf88a5ed7c9d39c1ea
Nikita <nkdevinfo@gmail.com>
2020-04-16 13:26:07 +0300

    initial commit

---
+++ eslint/README.md
@@ -0,0 +1,25 @@
+# eslint-config-nk
+
+## Installation
+
+```sh
+npm i eslint-config-nk -D
+```
+
+## Usage
+
+Add the config to your `.eslintrc`:
+
+```json
+{
+  "extends": "eslint-config-nk"
+}
+```
+
+or `.eslintrc.js`:
+
+```js
+module.exports = {
+  extends: 'eslint-config-nk',
+}
+```
---
+++ eslint/index.js
@@ -0,0 +1,53 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      defaultParams: true,
+      spread: true,
+    },
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    'func-call-spacing': 'error',
+    'eol-last': 'error',
+    'semi': 'error',
+    'semi-spacing': 'error',
+    'space-before-blocks': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
+    'space-in-parens': ['error', 'always'],
+    'space-infix-ops': ['error', { 'int32Hint': false }],
+    'space-unary-ops': [
+        'error',
+        {
+            'overrides': {
+                '!': true
+            }
+        }
+    ],
+
+    // rules for https://github.com/benmosher/eslint-plugin-import
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/prefer-default-export': 0,
+
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+  },
+}
